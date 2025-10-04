52f4e81af92b304a3bc75176ff7e0abdafdffc3a
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-04 21:28:40 +0500

    Task1

---
+++ Tyuiu.DolganovAV.Sprint3.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DolganovAV.Sprint3.Task1.V16.Lib;
+namespace Tyuiu.DolganovAV.Sprint3.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.7;
+            int startValue = 1;
+            int stopValue = 15;
+
+            Assert.AreEqual(15.948, ds.GetSumSeries(value, startValue, stopValue));
+        }
+    }
+}
