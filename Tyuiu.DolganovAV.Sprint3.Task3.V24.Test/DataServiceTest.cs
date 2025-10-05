9388ea751f92aa5b010f88a113aa279c72d70141
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-05 13:35:00 +0500

    Task3

---
+++ Tyuiu.DolganovAV.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DolganovAV.Sprint3.Task3.V24.Lib;
+namespace Tyuiu.DolganovAV.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharInString()
+        {
+            DataService ds = new DataService();
+            string value = "gft hggt ntg";
+            char replaceable = 'g';
+            char replacement = '*';
+            Assert.AreEqual("*ft h**t nt*", ds.ReplaceCharInString(value, replaceable, replacement));
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint3.Task3.V24/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.DolganovAV.Sprint3.Task3.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, используя цикл foreach, которая заменяет буквы g    *");
+        Console.WriteLine("* в строке на *                                                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '*';
+
+        Console.WriteLine($"Исходная строка = {value}");
+        Console.WriteLine($"Заменяемый символ = {replaceable}");
+        Console.WriteLine($"Замена символа = {replacement}");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ReplaceCharInString(value, replaceable, replacement));
+        Console.ReadKey();
+    }
+}
