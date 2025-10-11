3d5b12d947e5c9268e1f6ccf4b457f449dac19cc
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-11 13:13:26 +0500

    Task7

---
+++ Tyuiu.DolganovAV.Sprint3.Task7.V3.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DolganovAV.Sprint3.Task7.V3.Lib
+{
+        public class DataService : ISprint3Task7V3
+        {
+            public double[] GetMassFunction(int startValue, int stopValue)
+            {
+                double[] valueArray;
+                int len = stopValue - startValue + 1;
+                valueArray = new double[len];
+                double y;
+                int count = 0;
+                for (int x = startValue; x <= stopValue; x++)
+                {
+                    var numerator = (3 * x - 1.5);
+                    var denominator = (Math.Sin(x) - 3 + x);
+                    if (Math.Abs(denominator) < 0) y = 0;
+                    else y = Math.Round((numerator / denominator) + 2, 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+                return valueArray;
+            }
+        }
+}
---
+++ Tyuiu.DolganovAV.Sprint3.Task7.V3/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.DolganovAV.Sprint3.Task7.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции на         *");
+        Console.WriteLine("* заданном диапазоне [-5; 5] с шагом 1                                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine($"Начало отрезка = {startValue}");
+        Console.WriteLine($"Конец отрезка = {stopValue}");
+
+        double[] valueArray = ds.GetMassFunction(startValue, stopValue);
+        int len = valueArray.Length;
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |   f(x)   |");
+        Console.WriteLine("+----------+----------+");
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1,6:f2}  |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
